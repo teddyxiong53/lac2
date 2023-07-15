7eda0d1737fdd7d045931c1df19de737eb9e6e73
teddyxiong53 <1073167306@qq.com>
2023-07-15 16:06:08 +0800

    test-fscript可以编译运行。

--- README.md
+++ README.md
@@ -4,3 +4,25 @@
 
 主要基于awtk的tkc来做。
 
+编译方法：
+
+```
+xmake 
+```
+
+运行：
+
+```
+xmake run test
+```
+
+# test程序
+
+## test-fscript
+
+这个是解释执行fscript的例子。
+
+```
+xmake run test-fscript @./test/hello.fs
+```
+
--- xmake.lua
+++ xmake.lua
@@ -6,11 +6,11 @@ add_rules("mode.debug", "mode.release")
 
 
 add_defines("LINUX", "HAS_PTHREAD", "HAS_STD_MALLOC", "HAVE_CLOCK_GETTIME", "HAS_SEM_OPEN", "HAVE_SEM_TIMEDWAIT",
-"HAS_GET_TIME_US64"
+"HAS_GET_TIME_US64", "WITH_DATA_READER_WRITER", "HAS_STDIO"
 )
 
-add_includedirs("./")
-add_includedirs("./3rd")
+add_includedirs("./src")
+add_includedirs("./3rd", "./3rd/mbedtls/include")
 
 add_links("c","m","rt", "pthread", "dl")
 
@@ -19,9 +19,17 @@ target("lac")
     -- set kind
     set_kind("shared")
     -- add deps
-    add_files("tkc/*.c")
-    add_files("platforms/pc/*.c")
-
+    add_files("src/tkc/*.c")
+    add_files("src/platforms/pc/*.c")
+    add_files("src/fscript_ext/*.c")
+    add_files("src/streams/*/*.c")
+    add_files("src/debugger/*.c")
+    add_files("src/ubjson/*.c")
+    add_files("src/conf_io/*.c")
+    add_files("src/xml/*.c")
+    add_files("src/compressors/*.c")
+    add_files("3rd/mbedtls/library/*.c")
+    add_files("3rd/miniz/*.c")
 includes(
 "test"
 )
